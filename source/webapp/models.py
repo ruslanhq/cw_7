55b2f1bf834af1a797473179145731949951e714
Ruslan <h.ruslan97@gmail.com>
2019-10-19 11:40:12 +0600

    Added models Poll and Choice

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    question = models.CharField(max_length=100, verbose_name='Вопрос')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата и время создания')
+
+
+class Choice(models.Model):
+    text = models.CharField(max_length=50, verbose_name='Текст варианта')
+    poll = models.ForeignKey('webapp.Poll', related_name='choices', on_delete=models.CASCADE, verbose_name='Опрос')
+
