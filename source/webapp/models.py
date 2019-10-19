bbc3a1a7722f84c5338f0fdf5999e4128645b24a
Ruslan <h.ruslan97@gmail.com>
2019-10-19 12:14:00 +0600

    Added fixtures

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -2,11 +2,17 @@ from django.db import models
 
 
 class Poll(models.Model):
-    question = models.CharField(max_length=100, verbose_name='Вопрос')
+    question = models.TextField(max_length=100, verbose_name='Вопрос')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата и время создания')
+
+    def __str__(self):
+        return self.question
 
 
 class Choice(models.Model):
-    text = models.CharField(max_length=50, verbose_name='Текст варианта')
+    text = models.TextField(max_length=50, verbose_name='Текст варианта')
     poll = models.ForeignKey('webapp.Poll', related_name='choices', on_delete=models.CASCADE, verbose_name='Опрос')
+
+    def __str__(self):
+        return self.text
 
